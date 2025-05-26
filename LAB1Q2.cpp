93da8bf0608c772c8061269d9e98aa89f48e79c2
Pratik-0000 <csit2081013_pratik@achsnepal.edu.np>
2025-05-26 20:16:06 +0545

    Add files via upload

---
+++ LAB1Q2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Enter two Numbers" << endl;
+	int a,b;
+	cin >> a >> b;
+	cout << "Sum = " << a+b << endl << "Difference = " << a-b << endl << " Product = " << a*b;
+	return 0;
+}

2e62c1d68ac3569e5c6c2cfbedffcaa56e561199
Pratik-0000 <csit2081013_pratik@achsnepal.edu.np>
2025-05-26 20:25:11 +0545

    Add files via upload

---
+++ LAB1Q4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	float sum,b;
+	cout << "Enter a integer value " << endl;
+	cin >> a;
+	cout << "Enter a Floating point value" << endl;
+	cin >> b;
+	sum = a+b;
+	cout << "sum is "<< sum;
+	return 0;
+}

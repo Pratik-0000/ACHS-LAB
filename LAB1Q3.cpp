90a9d77a76457ac7be6a60d49466094ef5143d3a
Pratik-0000 <csit2081013_pratik@achsnepal.edu.np>
2025-06-03 11:50:19 +0545

    Update LAB1Q3.cpp

--- LAB1Q3.cpp
+++ LAB1Q3.cpp
@@ -1,16 +1,11 @@
 #include<iostream>
 #define pi 3.14
 using namespace std;
-float enterradii()
+int main()
 {
 	cout << "Enter radius of circle"<< endl;
 	float r;
 	cin >> r;
-	return r;
-} 
-int main()
-{
-	float r = enterradii();
 	cout << "Area of circle is "<< pi*r*r << endl <<"Perimeter of circle is "<< 2*pi*r;
 	return 0;
 }
